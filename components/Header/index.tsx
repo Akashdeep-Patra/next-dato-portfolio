b00b16020d487f44fc1ecab024594be8d06b9e05
Akashdeep <adeep8961@gmail.com>
2021-11-14 15:24:50 +0530

    moon icon

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { HeaderIcon } from '../../Shared/SvgComponents';
 import { ImSun } from 'react-icons/im';
 import { useTheme } from 'next-themes';
-import { BsMoonStars } from 'react-icons/bs';
+import { MdNightsStay } from 'react-icons/md';
 export interface HeaderProps {}
 const Header: React.FC<HeaderProps> = () => {
   const { theme, setTheme } = useTheme();
@@ -14,8 +14,8 @@ const Header: React.FC<HeaderProps> = () => {
       borderRadius: ['20%', '20%', '50%', '50%', '20%'],
     },
     dark: {
-      scale: [1, 1.9, 1],
-      rotate: [0, 160, 0],
+      rotate: [0, 10, 0],
+      scale: [1, 1.5, 1.5, 1],
       borderRadius: ['20%', '20%', '50%', '50%', '20%'],
     },
   };
@@ -29,13 +29,14 @@ const Header: React.FC<HeaderProps> = () => {
       <motion.button
         animate={theme}
         variants={variants}
+        transition={{ type: 'spring', stiffness: 100 }}
         aria-label='Toggle Dark Mode'
         type='button'
         className='p-5 text-center flex flex-col justify-center rounded-[6px] dark:text-white text-black '
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       >
         {theme === 'light' && <ImSun size={40} className='' />}
-        {theme === 'dark' && <BsMoonStars size={40} className='' />}
+        {theme === 'dark' && <MdNightsStay size={40} className='' />}
       </motion.button>
     </motion.div>
   );
